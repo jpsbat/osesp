db5604572b5a95c0bc0be622d05f24baa6a4cd21
jpsbat <jpsbat56@gmail.com>
2024-03-14 10:52:39 -0300

    cadastrar permissao

--- src/pages/Permissao.vue
+++ src/pages/Permissao.vue
@@ -11,15 +11,13 @@
 
             <md-card-content>
               <div class="md-layout">
-                <div class="md-layout-item md-size-33 mt-3">
-                  <select id="select1" class="form-select">
-                    <option disabled selected>Cargo</option>
-                    <option>Administrador</option>
-                    <option>Operação</option>
-                    <option>TI</option>
-                  </select>
+                <div class="md-layout-item md-size-33 mt-2">
+                  <md-field>
+                    <label>Cargo</label>
+                    <md-input v-model="cargo" type="text"></md-input>
+                  </md-field>
                 </div>
-                <div class="md-layout-item md-size-33 mt-3">
+                <div class="md-layout-item md-size-33 mt-4">
                   <select id="select1" class="form-select">
                     <option disabled selected>Administrador</option>
                     <option>Sim</option>
@@ -27,7 +25,13 @@
                   </select>
                 </div>
                 <div class="md-layout-item md-size-100 text-right">
-                  <md-button class="md-raised md-warning">Cadastrar</md-button>
+                  <md-button
+                    class="md-raised md-warning"
+                    type="submit"
+                    value="cadastrar"
+                    @click="cadastrarCliente($event)"
+                    >Cadastrar</md-button
+                  >
                 </div>
               </div>
             </md-card-content>
@@ -40,16 +44,99 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "permissao",
+  data() {
+    return {
+      cargo: "",
+      administrador: "",
+      permissaoacesso: [],
+      showModal: false,
+    };
+  },
   props: {
     dataBackgroundColor: {
       type: String,
       default: "",
     },
   },
-  data() {
-    return {};
+  mounted() {
+    this.listar();
+  },
+  methods: {
+    cadastrarPermissao(e) {
+      e.preventDefault();
+
+      axios
+        .post("http://localhost:3000/routes/permissaoacesso/cadastrar", {
+          cargo: this.cargo,
+          administrador: this.administrador,
+        })
+        .then((response) => {
+          console.log(response);
+          this.listar();
+          this.$swal("Sucesso!", `${cargo} cadastrado no sistema.`, "success");
+        })
+        .catch((error) => console.log(error));
+    },
+    listar() {
+      axios
+        .get("http://localhost:3000/routes/permissaoacesso/listar")
+        .then((response) => {
+          this.permissaoacesso = response.data.data;
+        })
+        .catch((error) => console.log(error));
+    },
+    atualizarCliente(id) {
+      axios
+        .get(`http://localhost:3000/routes/permissaoacesso/${id}`)
+        .then((response) => {
+          var novo_cargo = window.prompt(
+            `Cargo: ${cargo}`,
+            response.data.data.cargo
+          );
+          var novo_administrador = window.prompt(
+            `Administrador: ${administrador}`,
+            response.data.data.administrador
+          );
+
+          if (novo_cargo && novo_administrador !== null) {
+            axios
+              .patch(
+                `http://localhost:3000/routes/permissaoacesso/alterar/${id}`,
+                {
+                  cargo: novo_cargo,
+                  administrador: novo_administrador,
+                }
+              )
+              .then((response) => {
+                console.log(response);
+                this.listar();
+              })
+              .catch((error) => console.log(error));
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+    confirmarExclusaoPermissaoAcesso(id, cargo, administrador) {
+      this.idPermissaoAcesso = id;
+      this.novoCargo = cargo;
+      this.novoAdministrador = administrador;
+      this.showModal = true;
+    },
+    excluirPermissaoAcesso(id) {
+      axios
+        .delete(`http://localhost:3000/routes/permissaoacesso/excluir/${id}`)
+        .then((response) => {
+          console.log(response);
+          this.listar();
+        })
+        .catch((error) => console.log(error));
+
+      this.showModal = false;
+    },
   },
 };
 </script>
